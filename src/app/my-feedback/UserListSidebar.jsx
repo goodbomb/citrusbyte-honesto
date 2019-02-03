187d961630347c2b5e3af964881ee79a9588a291
Ben Scott <ben@goodbomb.com>
2019-02-02 20:15:54 -0800

    Added sidebar title for My Feedback section

--- src/app/my-feedback/UserListSidebar.jsx
+++ src/app/my-feedback/UserListSidebar.jsx
@@ -6,11 +6,21 @@ import UserListSidebarItem from './UserListSidebarItem';
 const StyledUserList = styled.ul`
     margin: 0;
     padding: 0;
+
+    .sidebar-title {
+        border-bottom: solid 1px #d9dcde;
+        color: #59636e;
+        font-size: 12px;
+        letter-spacing: 0.15em;
+        padding: 12px;
+        text-transform: uppercase;
+    }
 `;
 
 const UserListSidebar = ({ users }) => {
     return (
         <StyledUserList className="user-list">
+            <div className="sidebar-title">Feedback Received</div>
             {users.map((user) =>
                 <UserListSidebarItem key={user.id} user={user} />
             )}
